98cef685684bfb1d33c37c7e9386cf0e5b218e2f
Patrick Haidinger <patrickhaidinger@j009mac18.local>
2018-12-20 11:10:22 +0100

    first commit

---
+++ js/spriteList.js
@@ -0,0 +1,26 @@
+class spriteList{
+
+/**
+*Konstruktor um eine Sprite Liste zu erstellen
+*/
+
+constructor (){
+
+    this.elements = new Array();
+
+}
+/** füge Elemente zur Liste hinzu
+* @param sp sprite für die Liste
+*/
+addSprite(sp){ 
+this.elements.push(sp);
+
+}
+update (){
+for(var i=0;i<this.elements.length;i++){
+this.elements[i].update();
+};
+
+}
+
+}
